6a9d3939cca4be1b678246b1ae5514780367e198
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2017-01-07 00:12:25 +0100

    initial commit - osnovni scrapping naslova postova nanaslovnici

---
+++ pollitika.com Analyzer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScrapySharp.Extensions;
+using ScrapySharp.Network;
+
+namespace pollitika.com_Analyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ScrapingBrowser Browser = new ScrapingBrowser();
+            Browser.AllowAutoRedirect = true; // Browser has settings you can access in setup
+            Browser.AllowMetaRedirect = true;
+
+            WebPage PageResult = Browser.NavigateToPage(new Uri("http://www.pollitika.com"));
+
+            List<string> Names = new List<string>();
+            var Table = PageResult.Html.CssSelect(".node");
+
+            foreach (var post in Table)
+            {
+                Console.WriteLine("New");
+                foreach (var row in post.SelectNodes("h1"))
+                {
+                    string s = row.InnerText;
+
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}

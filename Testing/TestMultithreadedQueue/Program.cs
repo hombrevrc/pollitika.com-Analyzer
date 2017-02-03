83fa4599a54c938462881ce49350526413808ac5
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2017-02-04 00:13:01 +0100

    added Tets Multithreaded Queue project

---
+++ Testing/TestMultithreadedQueue/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestMultithreadedQueue
+{
+    class Program
+    {
+        static Random rnd = new Random(5);
+
+        public static void AnalyzePost(string inUrl)
+        {
+            Console.WriteLine("Starting " + inUrl);
+            Thread.Sleep(rnd.Next(500));
+        }
+
+        static void Main(string[] args)
+        {
+            // generirati listu od 100 stringova
+            List<string> listUrls = new List<string>();
+            for(int i=0; i<100; i++)
+                listUrls.Add("Post " + i.ToString());
+
+            int workThr, compl;
+            ThreadPool.GetMaxThreads(out workThr, out compl);
+        }
+    }
+}

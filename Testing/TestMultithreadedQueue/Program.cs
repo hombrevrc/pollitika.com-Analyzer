56ccdcfe43b02fe199a1c5e3c0ffccc25e10de84
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2017-02-05 00:03:24 +0100

    implemented multithreaded queue in test program

--- Testing/TestMultithreadedQueue/Program.cs
+++ Testing/TestMultithreadedQueue/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace TestMultithreadedQueue
 {
+    
     class Program
     {
         static Random rnd = new Random(5);
@@ -14,18 +16,68 @@ namespace TestMultithreadedQueue
         public static void AnalyzePost(string inUrl)
         {
             Console.WriteLine("Starting " + inUrl);
-            Thread.Sleep(rnd.Next(500));
+            Thread.Sleep(1500 + rnd.Next(5000));
+        }
+
+        public static void WebCrawl(Func<string> getNextUrlToCrawl, // returns a URL or null if no more URLs 
+                                    Action<string> crawlUrl, // action to crawl the URL 
+                                    int pauseInMilli // if all threads engaged, waits for n milliseconds
+        )
+        {
+            const int maxQueueLength = 5;
+            string currentUrl = null;
+            int queueLength = 0;
+
+            while ((currentUrl = getNextUrlToCrawl()) != null)
+            {
+                repeat:
+                //Console.WriteLine("Queue length {0}", queueLength);
+
+                string temp = currentUrl;
+                if (queueLength < maxQueueLength)
+                {
+                    var url = currentUrl;               // needed for closure capture
+                    Interlocked.Increment(ref queueLength);
+                    Task.Factory.StartNew(() =>
+                                                {
+                                                    crawlUrl(temp);
+                                                } )
+                                .ContinueWith((t) =>{
+                                                        if (t.IsFaulted)
+                                                            Console.WriteLine(t.Exception.ToString());
+                                                        else
+                                                            Console.WriteLine("Successfully done " + url);
+                                                        Interlocked.Decrement(ref queueLength);
+                                                    }
+                                                );
+                }
+                else
+                {
+                    //  Console.WriteLine("Waiting");
+                    Thread.Sleep(pauseInMilli);
+
+                    goto repeat;
+
+                }
+            }
         }
 
         static void Main(string[] args)
         {
             // generirati listu od 100 stringova
             List<string> listUrls = new List<string>();
-            for(int i=0; i<100; i++)
+            for(int i=0; i<23; i++)
                 listUrls.Add("Post " + i.ToString());
+            listUrls.Add(null);
 
             int workThr, compl;
             ThreadPool.GetMaxThreads(out workThr, out compl);
+
+            Random r = new Random();
+            int j = 0;
+            WebCrawl(() => listUrls[j++],
+                (url) => AnalyzePost(url),
+                500);
         }
     }
 }

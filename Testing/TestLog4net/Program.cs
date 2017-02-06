bb589e49a6da22654a21787b0cd45c6e8a63ec6f
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2017-02-06 11:12:43 +0100

    added test project for log4net

---
+++ Testing/TestLog4net/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Repository.Hierarchy;
+
+namespace TestLog4net
+{
+    class Program
+    {
+        //private static readonly ILog Logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        private static void Main(string[] args)
+        {
+            log4net.Config.BasicConfigurator.Configure();
+            ILog log = log4net.LogManager.GetLogger(typeof(Program));
+            log.Debug("This is a debug message");
+            log.Warn("This is a warn message");
+            log.Error("This is a error message");
+            log.Fatal("This is a fatal message");
+
+            log.InfoFormat("Running as {0}", WindowsIdentity.GetCurrent().Name);
+            log.Error("This will appear in red in the console and still be written to file!");
+        }
+    }
+}

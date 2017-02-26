f06d7ec3868326601fee2f7892db91dc53641745
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2017-02-27 00:30:09 +0100

    reorganized code to AnalyzerLib

---
+++ pollitika.com_AnalyzerLib/UserPostsAnalyzer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using HtmlAgilityPack;
+using log4net;
+using ScrapySharp.Extensions;
+using ScrapySharp.Network;
+
+namespace pollitika.com_AnalyzerLib
+{
+    public class UserPostsAnalyzer
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        // return list of URLs of posts for given user
+        public static List<string> GetListOfUserPosts(string userName)
+        {
+            List<string> retList = new List<string>();
+
+            ScrapingBrowser Browser = new ScrapingBrowser();
+            Browser.AllowAutoRedirect = true;                       // Browser has settings you can access in setup
+            Browser.AllowMetaRedirect = true;
+            Browser.Encoding = Encoding.UTF8;
+
+            // first, we have to get total number of pages
+            int pageCount   = 0;
+            string pageUrl1 = "http://www.pollitika.com/blog/" + userName;
+            WebPage startPage = Browser.NavigateToPage(new Uri(pageUrl1));
+            HtmlNode mainContent = startPage.Html.Descendants().Where(x => x.Id == "content-main").First();
+
+            var itemlist = mainContent.Descendants().Where(n => n.GetAttributeValue("class", "").Equals("pager")).ToList();
+            if (itemlist.Count > 0)
+            {
+                string s = itemlist[0].LastChild.PreviousSibling.InnerHtml;
+
+                int n1 = s.IndexOf("?page=");
+                int n2 = s.IndexOf("\"", n1);
+                string num = s.Substring(n1 + 6, n2 - n1 - 6);
+
+                pageCount = Convert.ToInt32(num);
+            }
+
+            log.Info("Getting posts for user " + userName + " Number of pages " + pageCount.ToString());
+
+            int pageIndex = 0;
+            while (pageIndex <= pageCount)
+            {
+                string pageUrl = pageIndex <= 0 ? "http://www.pollitika.com/blog/" + userName : "http://pollitika.com/blog/" + userName + "?page=" + pageIndex.ToString();
+
+                try
+                {
+                    log.Debug("Doing page " + pageUrl);
+
+                    WebPage PageResult = Browser.NavigateToPage(new Uri(pageUrl));
+
+                    List<string> Names = new List<string>();
+                    var Table = PageResult.Html.CssSelect(".node");
+
+                    foreach (var post in Table)
+                    {
+                        var a = post.CssSelect(".first");
+
+                        foreach (var row in post.SelectNodes("h1"))
+                        {
+                            string title = row.InnerText;
+                            string html = row.InnerHtml;
+
+                            int start = html.IndexOf("href=\"");
+                            int end = html.IndexOf("\">");
+
+                            string href = html.Substring(start + 6, end - start - 6);
+
+                            retList.Add(href);
+                        }
+                    }
+
+                    pageIndex++;
+                }
+                catch (Exception e)
+                {
+                    log.Error("Exception while getting user posts. Msg: " + e.Message);
+                    break;
+                }
+            }
+
+            return retList;
+        }
+    }
+}
---
+++ pollitika.com_AnalyzerLib/Utility.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using ScrapySharp.Html.Forms;
+using ScrapySharp.Network;
+
+namespace pollitika.com_AnalyzerLib
+{
+    public class Utility
+    {
+        public static ScrapingBrowser GetLoggedBrowser()
+        {
+            ScrapingBrowser Browser = new ScrapingBrowser();
+            Browser.AllowAutoRedirect = true; // Browser has many settings you can access in setup
+            Browser.AllowMetaRedirect = true;
+            Browser.Encoding = Encoding.UTF8;
+
+            //go to the home page
+            WebPage PageResult = Browser.NavigateToPage(new Uri("http://www.pollitika.com"));
+
+            PageWebForm form = PageResult.FindFormById("user-login-form");
+            // assign values to the form fields
+            form["name"] = "Zvone Radikalni";
+            form["pass"] = "economist0";
+            form.Method = HttpVerb.Post;
+            WebPage resultsPage = form.Submit();
+
+            return Browser;
+        }
+        public static DateTime ExtractDateTime(string inStr)
+        {
+            // extracting date
+            DateTime dt1 = new DateTime();
+            var regexDate = new Regex(@"\b\d{2}/\d{2}/\d{4}\b");
+            foreach (Match m in regexDate.Matches(inStr))
+            {
+                if (DateTime.TryParseExact(m.Value, "dd/MM/yyyy", null, DateTimeStyles.None, out dt1))
+                {
+                    break;
+                }
+            }
+            // extracting time
+            int hh = 0, mm = 0;
+            var regexTime = new Regex(@"\b\d{2}:\d{2}\b");
+            foreach (Match m in regexTime.Matches(inStr))
+            {
+                var values = m.Value.Split(':');
+                hh = Convert.ToInt32(values[0]);
+                mm = Convert.ToInt32(values[1]);
+            }
+
+            DateTime retDate = new DateTime(dt1.Year, dt1.Month, dt1.Day, hh, mm, 0);
+
+            return retDate;
+        }
+
+        public static DateTime ExtractDateTime2(string inStr)
+        {
+            // extracting date
+            DateTime dt1 = new DateTime();
+            var regexDate = new Regex(@"\b\d{2}.\d{2}.\d{4}\b");
+            foreach (Match m in regexDate.Matches(inStr))
+            {
+                if (DateTime.TryParseExact(m.Value, "dd.MM.yyyy", null, DateTimeStyles.None, out dt1))
+                {
+                    break;
+                }
+            }
+            // extracting time
+            int hh = 0, mm = 0;
+            var regexTime = new Regex(@"\b\d{2}:\d{2}\b");
+            foreach (Match m in regexTime.Matches(inStr))
+            {
+                var values = m.Value.Split(':');
+                hh = Convert.ToInt32(values[0]);
+                mm = Convert.ToInt32(values[1]);
+            }
+
+            DateTime retDate = new DateTime(dt1.Year, dt1.Month, dt1.Day, hh, mm, 0);
+
+            return retDate;
+        }
+    }
+}
